db99380f46f4a9b8af071b1a5776c664b2a42e4c
= <=>
2023-08-24 11:30:58 +0100

    fixed error in checkout page with orders that have multiple products

--- pages/shoppingcart.vue
+++ pages/shoppingcart.vue
@@ -27,6 +27,8 @@
             <div class="text-2xl font-bold mb-2">
               Shopping Cart ({{ userStore.cart.length }})
             </div>
+
+            <p>Select the items to checkout with</p>
           </div>
 
           <div class="bg-[#FEEEEF] rounded-lg p-4 mt-4">
